ab3b2d7108a96c7bf8c6e795d46224a75745cd9d
akharkhalis <anton.harhalis@gmail.com>
2020-02-13 12:36:29 +0200

    palindrome

--- Leetcode_Palindrome Number.py
+++ Leetcode_Palindrome Number.py
@@ -1,6 +1,5 @@
-def isPalindrome( x):
+def isPalindrome(x):
     if x < 0:
-        print('isnot palindrome')
         return False
 
     revers = 0
@@ -9,23 +8,15 @@ def isPalindrome( x):
         last = x % 10
         x = int(x / 10)
         revers = revers * 10 + last
-        print('x ', x)
-        print('last ', int(last))
-        print('revers ', revers)
-        print('_______________next iteration')
         if revers == 0:
             if x == revers or x == int(revers / 10):
-                print('is palindrome')
                 return True
             else:
-                print('isnot palindrome')
                 return False
 
     if x == revers or x == int(revers / 10):
-        print('is palindrome')
         return True
     else:
-        print('isnot palindrome')
         return False
 
 isPalindrome(10)
